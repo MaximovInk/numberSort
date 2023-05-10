8aa20d9d26bcd6d0ac6d3585f50e5545bf4d60f8
MaximovInk <38013889+MaximovInk@users.noreply.github.com>
2023-05-11 02:02:46 +0300

    menu + hypercasual ui

---
+++ Assets/NumberSort/Scripts/InGameMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    public void HomeAction()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+}
